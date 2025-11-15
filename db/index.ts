524508403ca6710d8d99ab31c1392f38d6925a69
oovaa <omarvx211@gmail.com>
2025-11-15 19:33:36 +0200

    from local host to container name at 2025-11-15

--- db/index.ts
+++ db/index.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 
 export default async function init() {
-  mongoose.connect('mongodb://127.0.0.1:27017/node_mongo_crud')
+  mongoose.connect('mongodb://mongo-db:27017/node_mongo_crud')
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
