a6b5bbfdd04695d18ec6e7c578658bf1e5a22790
oovaa <omarvx211@gmail.com>
2025-11-16 21:16:56 +0200

    making com:con ports match at 2025-11-16

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
     proxy: {
       // Proxy API requests to backend running on localhost:3000
       '/api': 'http://143.110.226.0:3232/',
